52c8b2f6ac19df9060c20c23620eebfe8d58895d
Mackenzie McClane <aaritak@gmail.com>
2017-05-20 04:25:45 -0500

    Add tons of comments

--- create-tile.bat
+++ create-tile.bat
@@ -1,10 +1,14 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+rem Check if called with no arguments
 set args=%*
 if NOT DEFINED args (
+	rem Might as well see if they want an easier way to use this script
 	set /p choice="Add 'Create Custom Tile' to context menu? (y/n): "
 	if "!choice!" EQU "y" (
+		
+		rem Write the registry changes to a .reg file
 		set basedir=%~dp0
 		set tempfile=!basedir!custom-tiles.reg
 		
@@ -30,10 +34,9 @@ if NOT DEFINED args (
 		echo [HKEY_CURRENT_USER\Software\Classes\batfile\shell\CreateCustomTile\command] >> "!tempfile!"
 		echo @="\"!basedir:\=\\!create-tile.bat\" \"%%1\"" >> "!tempfile!"
 		
-		echo !tempfile!
-		
+		rem Run and delete the temporary .reg file
 		start !tempfile!
-		echo del !tempfile!
+		del !tempfile!
 	)
 	pause
 	exit
@@ -61,6 +64,10 @@ if '%1' NEQ 'am_admin' (
 
 echo Creating a custom tile for the file: %basefile%
 
+rem Clean up the input, separate it to three variables
+rem   basefile (file without extension)
+rem   basedir (file's folder)
+rem   baseext (file's extension)
 set basefile=%basefile:"=%
 set basefile=%basefile:/=\%
 for /f %%i in ("%basefile: =--%") do (
@@ -73,32 +80,46 @@ for /f %%i in ("%basefile: =--%") do (
 	set basefile=!basedir!!basefile!
 )
 
+rem If the extension isn't .exe, we create an exe that launches their file
 if "%baseext%" NEQ ".exe" (
+	
 	echo Start-Process "%basefile%%baseext%" > launch.ps1
+	rem Uses Ps2exe
 	powershell -executionpolicy remotesigned -File %cwd%\ps2exe.ps1 -inputFile launch.ps1 "%basefile%.exe" > nul
+	
+	rem Cleanup
 	del launch.ps1
 	del "%basefile%.exe.config"
 )
 
+rem The path to the VisualElementsManifest file
 set visualfile=%basefile%.VisualElementsManifest.xml
 
 
+rem Getting the logo/image for the tile
 set /p image="Image: "
+rem Get the extension of the image they requested
 for %%i in ("%image%") do set imageext=%%~xi
 
+rem Create a new path for the image, using the exe's folder and the image's extension
 set imagefile=%basefile%.tile%imageext%
 
+rem Copy their requested icon to the exe's folder
 copy /y "%image%" "%imagefile%" > nul
 
+rem Remove the directory path from the image path, making it relative, instead of absolute
 for /f %%i in ("%imagefile: =--%") do (
 	set imagefile=%%~nxi
 	set imagefile=!imagefile:--= !
 )
 
+
+rem Configure a couple more VisualElements options
 set /p text="Text (light/dark): "
 set /p background="Background (hex color): #"
 
 
+rem Generate the VisualElementsManifest
 echo ^<?xml version='1.0' encoding='utf-8'?^> > "%visualfile%"
 echo ^<Application xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'^> >> "%visualfile%"
 echo ^<VisualElements >> "%visualfile%"
@@ -117,5 +138,3 @@ echo Square44x44Logo='%imagefile%'/^> >> "%visualfile%"
 echo ^</Application^> >> "%visualfile%"
 
 pause
-
-:end
